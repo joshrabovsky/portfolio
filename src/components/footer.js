f5ad9b4f3710c7ebc2f27d732783ec123c3a2a37
Josh Rabovsky <57297055+joshrabovsky@users.noreply.github.com>
2021-06-13 23:38:16 -0400

    responsive nav and footer fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,14 +6,14 @@ function Footer(){
           <li>
             <a href="https://www.linkedin.com/in/josh-rabovsky" target="_blank"  rel="noreferrer"><i className="fab fa-linkedin-in"></i></a>
           </li>
-          <li>|
+          <li><span>|</span>
             <a href="https://www.instagram.com/joshrabovsky/" target="_blank"  rel="noreferrer"><i className="fab fa-instagram"></i></a>
           </li>
-          <li>|
+          <li><span>|</span>
             <a href="https://www.facebook.com/josh.rabovsky/" target="_blank"  rel="noreferrer"><i className="fab fa-facebook"></i></a>
           </li>
-          <li>
-            |<a href="mailto:jarsky.75@gmail.com" target="_blank" rel="noreferrer"><i className="fas fa-envelope"></i></a>
+          <li><span>|</span>
+            <a href="mailto:jarsky.75@gmail.com" target="_blank" rel="noreferrer"><i className="fas fa-envelope"></i></a>
           </li>
         </ul>
         <p>&copy; Josh Rabovsky 2021 (Inspired by Mohit Gupta)</p>
