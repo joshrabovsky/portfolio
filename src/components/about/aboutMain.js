008c81369d898210fba99ab91d4eab7dfefffbc6
Josh Rabovsky <57297055+joshrabovsky@users.noreply.github.com>
2021-07-03 13:26:54 -0400

    Text updates

--- src/components/about/aboutMain.js
+++ src/components/about/aboutMain.js
@@ -7,16 +7,16 @@ function AboutMain() {
             <div className="content-column">
                 <h4>AMBITIOUS • RESULTS DRIVEN • PROBLEM SOLVER</h4>
                 <p>
-                Hi! I am third year student currently completing a software engineering and business dual degree through Western University and Ivey. I am a front end developer and machine learning enthusiast, who enjoys tackling difficult problems, learning new ideas, and discovering technologies. 
+                Hi! I am third year student currently completing a software engineering and business dual degree through Western University and Ivey. I am a front end developer, software developer, and aspriring full stack developer who enjoys tackling difficult problems, learning new ideas, and discovering technologies. 
                 </p>
                 <p>
-                As technology - in specific machine learning - continues to penetrate most fields, I am always on the lookout for opportunities that aim to connect business and technology. Fascinated by this intersection, I have learned more about the industries through internships as a Software Developer at Instant Record Check and as a Summer Intern for Connor, Clark, & Lunn Private Capital.
+                As technology - in specific machine learning - continues to penetrate the industry, I am always on the lookout for opportunities that aim to connect business and technology. Fascinated by this intersection, I have learned more about the industries through internships as a Software Developer at Instant Record Check and as a Summer Intern for Connor, Clark, & Lunn Private Capital.
                 </p>
                 <p>
-                During my free time I love to play basketball, travel, and eating pineapple pizza. Yes, you read that correctly.
+                During my free time I love to play basketball, travel, and eat pineapple pizza. Yes, you read that correctly.
                 </p>
                 <p>
-                    Thanks for checking out my portfolio! If you are interested, check out my resume below.
+                Thanks for checking out my portfolio! If you are interested, check out my resume below.
                 </p>
                 <div>
                     <a href="./Resume.pdf" className="highlight" download>Download Resume</a>
